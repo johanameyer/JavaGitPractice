4de87f13bc04b0c715ae3e32d2a55d23e18fc906
Johana Meyer <meyerjr@appstate.edu>
2023-01-28 00:22:45 -0500

    Cat classes that inherit HouseCat

---
+++ CatOne.java
@@ -0,0 +1,15 @@
+ class CatOne extends HouseCat
+{
+    @Override
+    public void introduce()
+    {
+        System.out.println("Kiwi is a tuxedo cat");
+    }
+
+    @Override
+    public void makeNoise()
+    {
+        System.out.println("Meeeeow");
+        
+    }
+}
---
+++ CatTwo.java
@@ -0,0 +1,17 @@
+ class CatTwo extends HouseCat
+{
+    @Override
+    public void introduce()
+    {
+        System.out.println("He is a white cat.");
+    }
+
+    @Override
+    public void makeNoise()
+    {
+        System.out.println("Mrooow");
+        
+    }
+}
+
+
